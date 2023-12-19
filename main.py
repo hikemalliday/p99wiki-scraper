a958000098994fa788b2fa1c8aef212bd1cf091e
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2023-12-19 09:55:45 -0600

    added wrapper for main.py function calls

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import npc_special
 import npc_zone
 import item_master
 import create_tables
+import asyncio
 
 if __name__ == "__main__":
     create_tables.create_tables()
     item_master.item_url_scrape()
-    item_master.item_master_scrape()
     npc_master.npc_url_scrape()
-    npc_master.npc_master_scrape()
-    npc_faction.npc_faction_scrape()
-    npc_item.npc_items_scrape()
-    npc_special.npc_special_scrape()
-    npc_zone.npc_zone_scrape()
+    asyncio.run(item_master.item_master_scrape())
+    asyncio.run(npc_master.npc_master_scrape())
+    asyncio.run(npc_faction.npc_faction_scrape())
+    asyncio.run(npc_item.npc_items_scrape())
+    asyncio.run(npc_special.npc_special_scrape())
+    asyncio.run(npc_zone.npc_zone_scrape())
 
